fe96b2cae2de6f49244e7b2f50b81b9818786ae3
Michael Larsen <mlarsen.c@xpanxion.com>
2023-07-13 08:00:08 -0700

    Added the book demo with two interfaces

--- 11_AbstractionAndInterfaces/InterfaceBookDemo/Program.cs
+++ 11_AbstractionAndInterfaces/InterfaceBookDemo/Program.cs
@@ -5,6 +5,9 @@ public interface IProduct
     double Price { get; set; }
     string Name { get; set; }
     string Color { get; set; }
+    string Author { get; set; }
+    int NumberOfPages { get; set; }
+    string ISBN { get; set; }
 }
 
 public class Book : IProduct
@@ -23,11 +26,17 @@ public class Customer
     {
         IProduct book = new Book();
         book.Name = "String Theory - A Modern Take";
+        book.Author = "Robert Graves";
+        book.NumberOfPages = 374;
         book.Color = "Green";
         book.Price = 74.99;
+        book.ISBN = "978312716541";
 
         Console.WriteLine("Book Details:");
         Console.WriteLine($"Name: {book.Name}");
+        Console.WriteLine($"Author: {book.Author}");
+        Console.WriteLine($"Number of Pages: {book.NumberOfPages}");
+        Console.WriteLine($"ISBN: {book.ISBN}");
         Console.WriteLine($"Color: {book.Color}");
         Console.WriteLine($"Price: {book.Price:C}");
 
