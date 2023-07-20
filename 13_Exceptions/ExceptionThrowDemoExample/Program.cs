e227907bebef793381e3df7ee9e1c3f1c43005bb
Michael Larsen <mlarsen.c@xpanxion.com>
2023-07-20 04:44:51 -0700

    Added Music Library App and Exception Examples

---
+++ 13_Exceptions/ExceptionThrowDemoExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandlingDemo
+{
+    class Program
+    {
+        public static void CreateFileRethrow()
+        {
+            string filePath = @"C:\csharp_class_test\resources.txt";
+
+            try
+            {
+                File.Create(filePath);
+            }
+            catch (IOException e)
+            {
+                // Rethrow the caught exception
+                throw;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                CreateFileRethrow();
+            }
+            catch (IOException e)
+            {
+                // Handle the rethrown exception or remove the catch block parameter if not needed.
+                Console.WriteLine("An error occurred while creating the file:");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
