fe9155da68cdbe8a5f606b8323cf365eeeaeed78
Noel Buxton <noel.buxton@mmc.com>
2023-07-25 16:59:51 +0100

    Created fileManagement

--- noel-code/14_FileIO/FileIOReadAndWriteDemoV4/Program.cs
+++ noel-code/14_FileIO/FileIOReadAndWriteDemoV4/Program.cs
@@ -7,36 +7,46 @@ namespace FileIOReadAndWriteDemoV4
     {
         static void Main(string[] args)
         {
+            FileIOReadAndWrite app = new FileIOReadAndWrite();
+
             // Create a working directory on our hard drive to read from and write to.
-            string directoryPath = @"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\FileIOReadAndWriteDemoV5";
+            string directoryPath = @"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\FileIOReadAndWriteDemoV4";
             Directory.CreateDirectory(directoryPath);
 
             // Call the WriteFile method to create and write to a file.
-            WriteFile();
+            WriteFile(directoryPath);
 
             // Display a message indicating that the file has been written successfully.
             Console.WriteLine("File has been written successfully!");
 
             // Call the ReadFile method to read from the file.
-            ReadFile();
+            ReadFile(directoryPath);
 
             // Display a message indicating that the file has been written successfully.
             Console.WriteLine("File has been displayed successfully!");
 
             // Call the CopyPasteText method to copy the content to a new file.
-            CopyPasteText();
+            CopyPasteText(directoryPath);
 
             // Display a message indicating that the content has been copied and pasted.
             Console.WriteLine("Content has been copied and pasted to a new file!");
 
             // Call the AppendTextToFile method to append text to an already created file
-            AppendTextToFile();
+            AppendTextToFile(directoryPath);
+
+            // Display a message indicating that the content has been copied and pasted.
+            Console.WriteLine("Content has been appended to an existing file!");
+
+            string myText = app.getInput("Please enter the text that you would like to append to the file");
+
+            // Call the AppendTextToFile method to append text to an already created file
+            AppendTextToFile(directoryPath, myText);
 
             // Display a message indicating that the content has been copied and pasted.
             Console.WriteLine("Content has been appended to an existing file!");
 
             // Call the ReadFile method to read from the file.
-            ReadFile();
+            ReadFile(directoryPath);
 
             // Display a message indicating that the file has been written successfully.
             Console.WriteLine("Content displays original and appended info!");
@@ -46,11 +56,11 @@ namespace FileIOReadAndWriteDemoV4
             Console.ReadKey();
         }
 
-        public static void WriteFile()
+        public static void WriteFile(string directoryPath)
         {
             try
             {
-                string filePath = Path.Combine(@"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\FileIOReadAndWriteDemoV4", "ReadAndWriteFromThisFile.txt");
+                string filePath = Path.Combine(directoryPath, "ReadAndWriteFromThisFile.txt");
 
                 using (StreamWriter writer = new StreamWriter(filePath))
                 {
@@ -69,11 +79,11 @@ namespace FileIOReadAndWriteDemoV4
             }
         }
 
-        public static void ReadFile()
+        public static void ReadFile(string directoryPath)
         {
             try
             {
-                string filePath = Path.Combine(@"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\FileIOReadAndWriteDemoV4", "ReadAndWriteFromThisFile.txt");
+                string filePath = Path.Combine(directoryPath, "ReadAndWriteFromThisFile.txt");
 
                 using (StreamReader reader = new StreamReader(filePath))
                 {
@@ -92,12 +102,12 @@ namespace FileIOReadAndWriteDemoV4
             }
         }
 
-        public static void CopyPasteText()
+        public static void CopyPasteText(string directoryPath)
         {
             try
             {
-                string sourceFilePath = Path.Combine(@"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\FileIOReadAndWriteDemoV4", "ReadAndWriteFromThisFile.txt");
-                string destinationFilePath = Path.Combine(@"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\FileIOReadAndWriteDemoV4", "CopyTextToThisFile.txt");
+                string sourceFilePath = Path.Combine(directoryPath, "ReadAndWriteFromThisFile.txt");
+                string destinationFilePath = Path.Combine(directoryPath, "CopyTextToThisFile.txt");
 
                 using (StreamReader reader = new StreamReader(sourceFilePath))
                 using (StreamWriter writer = new StreamWriter(destinationFilePath))
@@ -116,11 +126,11 @@ namespace FileIOReadAndWriteDemoV4
             }
         }
 
-        public static void AppendTextToFile()
+        public static void AppendTextToFile(string directoryPath)
         {
             try
             {
-                string filePath = Path.Combine(@"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\FileIOReadAndWriteDemoV4", "ReadAndWriteFromThisFile.txt");
+                string filePath = Path.Combine(directoryPath, "ReadAndWriteFromThisFile.txt");
 
                 // Use the StreamWriter constructor with the append parameter set to true.
                 // This will open the file in append mode, allowing new content to be added at the end of the file.
@@ -138,6 +148,32 @@ namespace FileIOReadAndWriteDemoV4
                 Console.WriteLine("An error occurred while appending to the file:");
                 Console.WriteLine(e.Message);
             }
+        }
+
+        public static void AppendTextToFile(string directoryPath, string textToAppend)
+        {
+            try
+            {
+                string filePath = Path.Combine(directoryPath, "ReadAndWriteFromThisFile.txt");
+
+                // Use the StreamWriter constructor with the append parameter set to true.
+                // This will open the file in append mode, allowing new content to be added at the end of the file.
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    // Use the WriteLine method to append data to the file.
+                    writer.WriteLine(textToAppend);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred while appending to the file:");
+                Console.WriteLine(e.Message);
+            }
+        }
+        public string getInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            return Console.ReadLine();
         }
     }
 }
