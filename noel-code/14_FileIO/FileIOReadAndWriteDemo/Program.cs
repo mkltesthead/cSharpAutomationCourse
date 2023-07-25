59fb7ed15e524db92b72dd4b518b8b0787e733ec
Noel Buxton <noel.buxton@mmc.com>
2023-07-25 14:03:49 +0100

    Added files

---
+++ noel-code/14_FileIO/FileIOReadAndWriteDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOReadAndWriteDemo
+{
+    class FileIOReadAndWrite
+    {
+        static void Main()
+        {
+            // Create a working directory on our hard drive to read from and write to.
+            string directoryPath = @"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\ReadAndWriteFromHere";
+            Directory.CreateDirectory(directoryPath);
+
+            // Call the writeFile method to create and write to a file.
+            WriteFile();
+
+            // Display a message indicating that the file has been written successfully.
+            Console.WriteLine("File has been written successfully!");
+
+            // Wait for user input before closing the console window.
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        public static void WriteFile()
+        {
+            try
+            {
+                // Combine the directory path and file name for the file.
+                string filePath = Path.Combine(@"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\ReadAndWriteFromHere", "ReadAndWriteFile.txt");
+
+                // Create a new instance of StreamWriter named writer.
+                // The StreamWriter class is used to write characters to a file.
+                // We use the "using" statement to automatically close the writer after writing.
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    // Use the Write method to write data to the file.
+                    // Each call to Write writes the data to the file without a new line.
+                    // Experiment with different formatting options such as New Lines or Spaces
+                    writer.WriteLine("Michael Larsen");
+                    writer.WriteLine("Victor Jones");
+                    writer.WriteLine("Brian Horban");
+                    writer.WriteLine("Lloyd Ellis");
+                    writer.WriteLine("Edward Scott");
+                }
+
+                // Note: The StreamWriter automatically flushes the buffer and writes the data to the file.
+
+                // Note: In C#, we use StreamWriter to handle buffering and write efficiently, so we don't need
+                // a separate BufferedWriter class like in Java.
+            }
+            catch (IOException e)
+            {
+                // If an exception occurs during file writing, print the error details.
+                Console.WriteLine("An error occurred while writing to the file:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ noel-code/14_FileIO/FileIOReadAndWriteDemoV3/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOReadAndWriteDemoV3
+{
+    class FileIOReadAndWrite
+    {
+        static void Main(string[] args)
+        {
+            // Create a working directory on our hard drive to read from and write to.
+            string directoryPath = @"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\FileIOReadAndWriteDemoV3";
+            Directory.CreateDirectory(directoryPath);
+
+            // Call the WriteFile method to create and write to a file.
+            WriteFile();
+
+            // Display a message indicating that the file has been written successfully.
+            Console.WriteLine("File has been written successfully!");
+
+            // Call the ReadFile method to read from the file.
+            ReadFile();
+
+            // Call the CopyPasteText method to copy the content to a new file.
+            CopyPasteText();
+
+            // Display a message indicating that the content has been copied and pasted.
+            Console.WriteLine("Content has been copied and pasted to a new file!");
+
+            // Wait for user input before closing the console window.
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        public static void WriteFile()
+        {
+            try
+            {
+                string filePath = Path.Combine(@"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\FileIOReadAndWriteDemoV3", "ReadAndWriteFromThisFile.txt");
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.Write("Lloyd Ellis \n");
+                    writer.Write("Brian Horban\n");
+                    writer.Write("Victor Jones\n");
+                    writer.Write("Michael Larsen\n");
+                    writer.Write("Edward Scott\n");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred while writing to the file:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void ReadFile()
+        {
+            try
+            {
+                string filePath = Path.Combine(@"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\FileIOReadAndWriteDemoV3", "ReadAndWriteFromThisFile.txt");
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    Console.WriteLine("Content of the file:");
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred while reading the file:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void CopyPasteText()
+        {
+            try
+            {
+                string sourceFilePath = Path.Combine(@"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\FileIOReadAndWriteDemoV3", "ReadAndWriteFromThisFile.txt");
+                string destinationFilePath = Path.Combine(@"C:\dev\SDET\cSharpAutomationCourse\noel-code\14_FileIO\FileIOReadAndWriteDemoV3", "CopyTextToThisFile.txt");
+
+                using (StreamReader reader = new StreamReader(sourceFilePath))
+                using (StreamWriter writer = new StreamWriter(destinationFilePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred while copying and pasting the content:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
