f1f94ca0f238669debbc66f5141b82f3cb903ae9
Michael Larsen <mlarsen.c@xpanxion.com>
2023-08-23 11:20:13 -0700

    Git ignore is working

--- 17_AutomatedTesting/NUnitDemoGeometryCalculator/Program.cs
+++ 17_AutomatedTesting/NUnitDemoGeometryCalculator/Program.cs
@@ -92,54 +92,55 @@
         }
     }
 
-
-    public class Circle
-    {
-        public static double CalculateArea(double radius)
+    /*
+        public class Circle
         {
-            return Math.PI * radius * radius;
+            public static double CalculateArea(double radius)
+            {
+                return Math.PI * radius * radius;
+            }
+
         }
 
-    }
-
-    public class Triangle
-    {
-        public static double CalculateArea(double baseLength, double height)
+        public class Triangle
         {
-            return 0.5 * baseLength * height;
-        }
-    }
-
-    public class Square
-    {
-        public static double CalculateArea(double side)
-        {
-            return side * side;
-        }
-    }
-
-    public class Parallelogram
-    {
-        public static double CalculateArea(double baseLength, double height)
-        {
-            return baseLength * height;
-        }
-    }
-
-    public class Pentagon
-    {
-        public static double CalculateArea(double side)
-        {
-            return 0.25 * Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5))) * side * side;
+            public static double CalculateArea(double baseLength, double height)
+            {
+                return 0.5 * baseLength * height;
+            }
         }
 
-    }
-
-    public class Hexagon
-    {
-        public static double CalculateArea(double side)
+        public class Square
         {
-            return 3 * Math.Sqrt(3) * side * side / 2;
+            public static double CalculateArea(double side)
+            {
+                return side * side;
+            }
         }
-    }
+
+        public class Parallelogram
+        {
+            public static double CalculateArea(double baseLength, double height)
+            {
+                return baseLength * height;
+            }
+        }
+
+        public class Pentagon
+        {
+            public static double CalculateArea(double side)
+            {
+                return 0.25 * Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5))) * side * side;
+            }
+
+        }
+
+        public class Hexagon
+        {
+            public static double CalculateArea(double side)
+            {
+                return 3 * Math.Sqrt(3) * side * side / 2;
+            }
+        } */
+
 }
