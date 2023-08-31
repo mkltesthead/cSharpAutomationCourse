b1b133d9037af2338e4e1151a653de4b35bb4160
Noel Buxton <noel.buxton@mmc.com>
2023-08-31 14:22:46 +0100

    Updated - trying to align with noel branch

---
+++ noel-code/GeometryCalcSolution/GeometryCalcApp/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using GeometryCalcLibrary;
+
+namespace GeometryCalcApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose a shape:");
+                Console.WriteLine("1. Circle");
+                Console.WriteLine("2. Triangle");
+                Console.WriteLine("3. Square");
+                Console.WriteLine("4. Parallelogram");
+                Console.WriteLine("5. Pentagon");
+                Console.WriteLine("6. Hexagon");
+                Console.WriteLine("7. Exit");
+
+                int choice;
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                    continue;
+                }
+
+                if (choice == 7)
+                {
+                    Console.WriteLine("Exiting the program...");
+                    break;
+                }
+
+                Console.WriteLine("Do you want to calculate Area (A) or Perimeter (P)?");
+                string calculationType = Console.ReadLine().ToLower();
+
+                if (calculationType != "a" && calculationType != "p")
+                {
+                    Console.WriteLine("Invalid input. Please enter 'A' for Area or 'P' for Perimeter.");
+                    continue;
+                }
+
+                double result;
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter the radius of the circle: ");
+                        double circleRadius = GetPositiveNumber();
+                        result = calculationType == "a" ? Circle.CalculateArea(circleRadius) : Circle.CalculatePerimeter(circleRadius);
+                        break;
+
+                    case 2:
+                        Console.Write("Enter the base of the triangle: ");
+                        double triangleBase = GetPositiveNumber();
+                        Console.Write("Enter the height of the triangle: ");
+                        double triangleHeight = GetPositiveNumber();
+                        result = calculationType == "a" ? Triangle.CalculateArea(triangleBase, triangleHeight) : Triangle.CalculatePerimeter(triangleBase, triangleHeight, triangleHeight);
+                        break;
+
+                    case 3:
+                        Console.Write("Enter the side length of the square: ");
+                        double squareSide = GetPositiveNumber();
+                        result = calculationType == "a" ? Square.CalculateArea(squareSide) : Square.CalculatePerimeter(squareSide);
+                        break;
+
+                    case 4:
+                        Console.Write("Enter the base of the parallelogram: ");
+                        double parallelogramBase = GetPositiveNumber();
+                        Console.Write("Enter the height of the parallelogram: ");
+                        double parallelogramHeight = GetPositiveNumber();
+                        result = calculationType == "a" ? Parallelogram.CalculateArea(parallelogramBase, parallelogramHeight) : Parallelogram.CalculatePerimeter(parallelogramBase, parallelogramHeight);
+                        break;
+
+                    case 5:
+                        Console.Write("Enter the side length of the pentagon: ");
+                        double pentagonSide = GetPositiveNumber();
+                        result = calculationType == "a" ? Pentagon.CalculateArea(pentagonSide) : Pentagon.CalculatePerimeter(pentagonSide);
+                        break;
+
+                    case 6:
+                        Console.Write("Enter the side length of the hexagon: ");
+                        double hexagonSide = GetPositiveNumber();
+                        result = calculationType == "a" ? Hexagon.CalculateArea(hexagonSide) : Hexagon.CalculatePerimeter(hexagonSide);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please select again.");
+                        continue;
+                }
+
+                Console.WriteLine(calculationType == "a" ? "The area is:" : "The perimeter (or circumference) is:");
+                Console.WriteLine(result);
+            }
+        }
+
+        static double GetPositiveNumber()
+        {
+            double number;
+            while (!double.TryParse(Console.ReadLine(), out number) || number <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive number.");
+            }
+            return number;
+        }
+    }
+}
---
+++ noel-code/GeometryCalcSolution/GeometryCalcTestsMSTest/UnitTest1.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GeometryCalcLibrary;
+using System.Security.Policy;
+
+namespace GeometryCalcTestsMSTest
+{
+    [TestClass]
+    public class CircleTests
+    {
+        [TestMethod]
+        public void CircleAreaCalculation()
+        {
+            double expectedArea = Math.PI * 5 * 5; // Assuming radius is 5
+            double actualArea = Circle.CalculateArea(5);
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod]
+        public void CirclePerimeterCalculation()
+        {
+            double expectedPerimeter = 2 * Math.PI * 5; // Assuming radius is 5
+            double actualPerimeter = Circle.CalculatePerimeter(5);
+            Assert.AreEqual(expectedPerimeter, actualPerimeter);
+        }
+    }
+
+    [TestClass]
+    public class TriangleTests
+    {
+        [TestMethod]
+        public void TriangleAreaCalculation()
+        {
+            double expectedArea = 10.0; // Assuming base length = 5 and height = 4
+            double actualArea = Triangle.CalculateArea(5, 4);
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod]
+        public void TrianglePerimeterCalculation()
+        {
+            double expectedPerimeter = 12.0; // Assuming side lengths are 4, 5, and 3
+            double actualPerimeter = Triangle.CalculatePerimeter(4, 5, 3);
+            Assert.AreEqual(expectedPerimeter, actualPerimeter);
+        }
+
+        [TestMethod]
+        public void TriangleRightAngled()
+        {
+            Assert.IsTrue(Triangle.IsRightAngled(3, 4, 5));
+            Assert.IsTrue(Triangle.IsRightAngled(3, 5, 4));
+            Assert.IsTrue(Triangle.IsRightAngled(4, 3, 5));
+            Assert.IsTrue(Triangle.IsRightAngled(4, 5, 3));
+            Assert.IsTrue(Triangle.IsRightAngled(5, 3, 4));
+            Assert.IsTrue(Triangle.IsRightAngled(5, 4, 3));
+
+            Assert.IsTrue(Triangle.IsRightAngled(-3, -4, -5));
+            Assert.IsTrue(Triangle.IsRightAngled(-3, -5, -4));
+            Assert.IsTrue(Triangle.IsRightAngled(-4, -3, -5));
+            Assert.IsTrue(Triangle.IsRightAngled(-4, -5, -3));
+            Assert.IsTrue(Triangle.IsRightAngled(-5, -3, -4));
+            Assert.IsTrue(Triangle.IsRightAngled(-5, -4, -3));
+        }
+
+        [TestMethod]
+        public void TriangleNotRightAngled()
+        {
+            Assert.IsFalse(Triangle.IsRightAngled(2, 4, 5));
+            Assert.IsFalse(Triangle.IsRightAngled(2, 5, 4));
+            Assert.IsFalse(Triangle.IsRightAngled(4, 2, 5));
+            Assert.IsFalse(Triangle.IsRightAngled(4, 5, 2));
+            Assert.IsFalse(Triangle.IsRightAngled(5, 2, 4));
+            Assert.IsFalse(Triangle.IsRightAngled(5, 4, 2));
+
+            Assert.IsFalse(Triangle.IsRightAngled(-2, -4, -5));
+            Assert.IsFalse(Triangle.IsRightAngled(-2, -5, -4));
+            Assert.IsFalse(Triangle.IsRightAngled(-4, -2, -5));
+            Assert.IsFalse(Triangle.IsRightAngled(-4, -5, -2));
+            Assert.IsFalse(Triangle.IsRightAngled(-5, -2, -4));
+            Assert.IsFalse(Triangle.IsRightAngled(-5, -4, -2));
+        }
+    }
+
+    [TestClass]
+    public class SquareTests
+    {
+        [TestMethod]
+        public void SquareAreaCalculation()
+        {
+            double expectedArea = 25; // Assuming side length is 5
+            double actualArea = Square.CalculateArea(5);
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod]
+        public void SquarePerimeterCalculation()
+        {
+            double expectedPerimeter = 20; // Assuming side length is 5
+            double actualPerimeter = Square.CalculatePerimeter(5);
+            Assert.AreEqual(expectedPerimeter, actualPerimeter);
+        }
+    }
+
+    [TestClass]
+    public class ParallelogramTests
+    {
+        [TestMethod]
+        public void ParallelogramAreaCalculation()
+        {
+            double expectedArea = 10 * 7; // Assuming base length is 10 and height is 7
+            double actualArea = Parallelogram.CalculateArea(10, 7);
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        [TestMethod]
+        public void ParallelogramPerimeterCalculation()
+        {
+            double expectedPerimeter = 2 * (10 + 7); // Assuming base length is 10 and height is 7
+            double actualPerimeter = Parallelogram.CalculatePerimeter(10, 7);
+            Assert.AreEqual(expectedPerimeter, actualPerimeter);
+        }
+    }
+    [TestClass]
+    public class PentagonTests
+    {
+        [TestMethod]
+        public void PentagonAreaCalculation()
+        {
+            double expectedArea = 0.25 * Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5))) * 7 * 7; // Assuming side length is 7
+            double actualArea = Pentagon.CalculateArea(7);
+            Assert.AreEqual(expectedArea, actualArea, 0.0001); // Adding a delta to account for potential floating-point errors
+        }
+
+        [TestMethod]
+        public void PentagonPerimeterCalculation()
+        {
+            double expectedPerimeter = 5 * 7; // Assuming side length is 7
+            double actualPerimeter = Pentagon.CalculatePerimeter(7);
+            Assert.AreEqual(expectedPerimeter, actualPerimeter, 0.0001); // Adding a delta to account for potential floating-point errors
+        }
+    }
+    [TestClass]
+    public class HexagonTests
+    {
+        [TestMethod]
+        public void HexagonAreaCalculation()
+        {
+            double expectedArea = 3 * Math.Sqrt(3) * 5 * 5 / 2; // Assuming side length is 5
+            double actualArea = Hexagon.CalculateArea(5);
+            Assert.AreEqual(expectedArea, actualArea, 0.0001); // Adding delta for floating-point precision
+        }
+
+        [TestMethod]
+        public void HexagonPerimeterCalculation()
+        {
+            double expectedPerimeter = 6 * 5; // Assuming side length is 5
+            double actualPerimeter = Hexagon.CalculatePerimeter(5);
+            Assert.AreEqual(expectedPerimeter, actualPerimeter);
+        }
+    }
+}
