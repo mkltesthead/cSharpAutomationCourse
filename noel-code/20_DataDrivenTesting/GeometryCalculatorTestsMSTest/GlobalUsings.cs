4cee4a3d1fd9268f2e743294898d3db8eca4228e
Noel Buxton <noel.buxton@mmc.com>
2023-09-01 13:08:46 +0100

    Added folder 20

---
+++ noel-code/20_DataDrivenTesting/GeometryCalculatorTestsMSTest/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using CsvHelper;
+global using CsvHelper.Configuration;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using System.Collections.Generic;
+global using System.Globalization;
+global using System.IO;
