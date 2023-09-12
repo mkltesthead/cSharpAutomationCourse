fa6c378cf43bec74dd36e353d26c39ce0c3f4f48
Michael Larsen <mlarsen.c@xpanxion.com>
2023-09-12 04:53:09 -0700

    Added Login Test Cases

--- 22_ElementInteractions/PlaywrightElementInteractions/MSTestCSSHRMOrangeLoginTest.cs
+++ 22_ElementInteractions/PlaywrightElementInteractions/MSTestCSSHRMOrangeLoginTest.cs
@@ -46,6 +46,85 @@ namespace PlaywrightElementInteractions
             Assert.IsNotNull(dashboardElement, "Dashboard");
         }
 
+        [TestMethod]
+        public async Task NegativeLoginToOrangeHRMInvalidCredentials()
+        {
+            // Navigate to the OrangeHRM login page
+            await _page.GotoAsync("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+            // Wait for the page to finish loading
+            await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+            // Wait for the username input field using a CSS Selector
+            await _page.WaitForSelectorAsync("input[name='username']");
+
+            // Locate and fill in the username input field using a CSS Selector
+            await _page.FillAsync("input[name='username']", "InvalidUser");
+
+            // Locate and fill in the password input field using a CSS Selector
+            await _page.FillAsync("input[name='password']", "invalidPassword");
+
+            // Click the "LOGIN" button using a CSS Selector
+            await _page.ClickAsync("button[type=submit]");
+
+            var alertContent = await _page.WaitForSelectorAsync(".oxd-alert-content-text");
+            Assert.IsTrue(await _page.QuerySelectorAsync(".oxd-alert-content-text") != null);
+            Console.WriteLine($" Alert Content: {alertContent}");
+
+        }
+
+        [TestMethod]
+        public async Task NegativeLoginToOrangeHRMNoUsername()
+        {
+            // Navigate to the OrangeHRM login page
+            await _page.GotoAsync("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+            // Wait for the page to finish loading
+            await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+            // Wait for the username input field using a CSS Selector
+            await _page.WaitForSelectorAsync("input[name='username']");
+
+            // Locate and fill in the username input field using a CSS Selector
+            await _page.FillAsync("input[name='username']", "");
+
+            // Locate and fill in the password input field using a CSS Selector
+            await _page.FillAsync("input[name='password']", "invalidPassword");
+
+            // Click the "LOGIN" button using a CSS Selector
+            await _page.ClickAsync("button[type=submit]");
+
+            var alertContent = await _page.WaitForSelectorAsync(".oxd-input-field-error-message");
+            Assert.IsTrue(await _page.QuerySelectorAsync(".oxd-input-field-error-message") != null);
+            Console.WriteLine($" Alert Content: {alertContent}");
+        }
+
+        [TestMethod]
+        public async Task NegativeLoginToOrangeHRMNoPassword()
+        {
+            // Navigate to the OrangeHRM login page
+            await _page.GotoAsync("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+            // Wait for the page to finish loading
+            await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+
+            // Wait for the username input field using a CSS Selector
+            await _page.WaitForSelectorAsync("input[name='username']");
+
+            // Locate and fill in the username input field using a CSS Selector
+            await _page.FillAsync("input[name='username']", "admin");
+
+            // Locate and fill in the password input field using a CSS Selector
+            await _page.FillAsync("input[name='password']", "");
+
+            // Click the "LOGIN" button using a CSS Selector
+            await _page.ClickAsync("button[type=submit]");
+
+            var alertContent = await _page.WaitForSelectorAsync(".oxd-input-field-error-message");
+            Assert.IsTrue(await _page.QuerySelectorAsync(".oxd-input-field-error-message") != null);
+            Console.WriteLine($" Alert Content: {alertContent}");
+        }
+
         [TestCleanup]
         public async Task TestCleanup()
         {
