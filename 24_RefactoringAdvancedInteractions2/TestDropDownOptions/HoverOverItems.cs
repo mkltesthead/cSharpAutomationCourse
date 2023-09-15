7cc83624d4bbd635acb1f711ac1990c1ce074897
Michael Larsen <mlarsen.c@xpanxion.com>
2023-09-15 05:18:40 -0700

    Refactored Examples

---
+++ 24_RefactoringAdvancedInteractions2/TestDropDownOptions/HoverOverItems.cs
@@ -0,0 +1,31 @@
+namespace ElementInteractionsMSTest
+{
+    [TestClass]
+    public class HoverOverItemsTest : TestBase
+    {
+        [TestMethod]
+        public async Task TestHover()
+        {
+            string url = BaseUrl + "hovers";
+            await Page.GotoAsync(url);
+
+            // Select all the avatar elements
+            var avatarElement = await Page.QuerySelectorAsync(".figure");
+
+            // Use the "hover" method of the element
+            await avatarElement.HoverAsync();
+
+            // Custom JavaScript evaluation to check visibility
+            var isFigcaptionVisible = await Page.EvaluateAsync<bool>(@"
+                () => {
+                const figcaption = document.querySelector('.figcaption');
+                return window.getComputedStyle(figcaption).getPropertyValue('display') !== 'none';
+                }
+                ");
+
+            // Assert the figcaption is visible
+            Console.WriteLine($"Hover action performed. isFigcaptionVisible: {isFigcaptionVisible}");
+            Assert.IsTrue(isFigcaptionVisible);
+        }
+    }
+}
---
+++ 24_RefactoringAdvancedInteractions2/TestDropDownOptions/KeyPresses.cs
@@ -0,0 +1,27 @@
+namespace ElementInteractionsMSTest
+{
+    [TestClass]
+    public class KeyPressTest : TestBase
+    {
+        [TestMethod]
+        public async Task KeyPress()
+        {
+            string url = BaseUrl + "key_presses";
+            await Page.GotoAsync(url);
+
+            await Page.Locator("#target").ClickAsync();
+
+            // Simulate a key press event (e.g., pressing the 'A' key)
+            await Page.Keyboard.PressAsync("a");
+
+            // Wait for a moment to allow any UI updates
+            await Page.WaitForTimeoutAsync(1000); // Adjust the delay as needed
+
+            // Use EvaluateAsync to get the text content of the result element
+            var resultText = await Page.EvaluateAsync<string>("() => document.getElementById('result').textContent");
+
+            // Assert that the result contains the expected key name ('a' in this case)
+            Assert.IsTrue(resultText.Contains("a", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
