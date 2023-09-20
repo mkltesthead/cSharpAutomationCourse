ff1bce0fc11cc5dd77c4673aaa1a17bfd7601cf4
mohit-ahuja <mohit.ahuja@jltgroupcloud.com>
2023-09-20 18:13:03 +0530

    commit

---
+++ 23_AdvancedInteractions/TestDropDownOptions/BaseClass.cs
@@ -0,0 +1,26 @@
+﻿global using Microsoft.Playwright;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace MSSampleProject
+{
+    [TestClass]
+    public class BaseClass
+    {
+        protected IPage Page;
+        protected string BaseUrl = "http://the-internet.herokuapp.com/";
+
+        [TestInitialize]
+        public async Task Initialize()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await Playwright.Chromium.LauchAsync();
+            Page = await browser.NewPageAsync();
+        }
+        [TestCleanup]
+        public async Task Cleanup()
+        {
+            await Page.CloseAsync();
+        }
+
+
+    }
+}
