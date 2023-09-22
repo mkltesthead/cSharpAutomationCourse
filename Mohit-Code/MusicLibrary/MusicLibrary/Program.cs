835c587ebb07869ad2d05b796a5b4deb899d9004
mohit-ahuja <mohit.ahuja@jltgroupcloud.com>
2023-09-22 21:43:01 +0530

    OnlineShoppingAndMusicLibraryCommit

---
+++ Mohit-Code/MusicLibrary/MusicLibrary/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Define the Song class to represent individual songs
+class Song
+{
+    public string SongTitle { get; }
+    public string Artist { get; }
+    public int DurationInSeconds { get; }
+
+    public Song(string songTitle, string artist, int durationInSeconds)
+    {
+        SongTitle = songTitle;
+        Artist = artist;
+        DurationInSeconds = durationInSeconds;
+    }
+}
+
+
+class Playlist
+{
+    public string Name { get; }
+    private List<Song> songsList = new List<Song>();
+
+    public Playlist(string name)
+    {
+        Name = name;
+    }
+
+    public void AddSong(Song song)
+    {
+        songsList.Add(song);
+    }
+
+    public void DisplaySongs()
+    {
+        Console.WriteLine($"Playlist: {Name}");
+        foreach (var song in songsList)
+        {
+            Console.WriteLine($"Song Title: {song.SongTitle}, Artist: {song.Artist},  Duration of Song: {song.DurationInSeconds} seconds");
+        }
+        Console.WriteLine();
+    }
+}
+
+// class to manage songs and playlists
+class Library
+{
+    private List<Song> musicLibrary = new List<Song>();
+    private List<Playlist> playlists = new List<Playlist>();
+
+    public void AddSong(Song song)
+    {
+        musicLibrary.Add(song);
+    }
+
+    public void CreatePlaylist(string name)
+    {
+        var playlist = new Playlist(name);
+        playlists.Add(playlist);
+    }
+
+    public void AddSongToPlaylist(string playlistName, Song song)
+    {
+        var playlist = playlists.FirstOrDefault(p => p.Name.ToLower().Equals(playlistName.ToLower()));
+        if (playlist != null)
+        {
+            playlist.AddSong(song);
+        }
+        else
+        {
+            Console.WriteLine($"Playlist '{playlistName}' not found.");
+        }
+    }
+
+    public void DisplayAllSongs()
+    {
+        Console.WriteLine("All Songs in the Music Library:");
+        foreach (var song in musicLibrary)
+        {
+            Console.WriteLine($"Song Title: {song.SongTitle}, Artist: {song.Artist},  Duration of Song: {song.DurationInSeconds} seconds");
+        }
+        Console.WriteLine();
+    }
+
+    public void DisplayAllPlaylists()
+    {
+        Console.WriteLine("All Playlists:");
+        foreach (var playlist in playlists)
+        {
+            playlist.DisplaySongs();
+        }
+    }
+
+    public List<Song> FindSongsByArtist(string artist)
+    {
+        return musicLibrary.Where(song => song.Artist.ToLower().Equals(artist.ToLower())).ToList();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Library musicLibrary = new Library();
+
+        // Adding sample songs
+        musicLibrary.AddSong(new Song("Calm Down", "Selena Gomez", 200));
+        musicLibrary.AddSong(new Song("Baby", "Justin Bieber", 250));
+        musicLibrary.AddSong(new Song("You Belong With Me", "Taylor Swift", 190));
+
+        // Creating playlists and adding songs to them
+        musicLibrary.CreatePlaylist("Playlist1");
+        musicLibrary.AddSongToPlaylist("Playlist1", new Song("Calm Down", "Selena Gomez", 200));
+        musicLibrary.AddSongToPlaylist("Playlist1", new Song("Baby", "Justin Bieber", 250));
+
+        // Displaying all songs in the music library
+        musicLibrary.DisplayAllSongs();
+
+        // Displaying all playlists and their associated songs
+        musicLibrary.DisplayAllPlaylists();
+
+        Console.WriteLine("Enter name of artist to search song:");
+        string artistName = Console.ReadLine();
+
+        // Search all songs by a specific artist
+        List<Song> songsByArtist = musicLibrary.FindSongsByArtist(artistName.ToLower());
+        if (songsByArtist.Count > 0)
+            Console.WriteLine($"All Songs by {artistName}:");
+        else 
+            Console.WriteLine($"Sorry!! No Song Exists by Artist {artistName}");
+
+        foreach (var song in songsByArtist)
+        {
+            Console.WriteLine($"Song Title: {song.SongTitle}, Duration of Song: {song.DurationInSeconds} seconds");
+        }
+    }
+}
---
+++ Mohit-Code/OnlineShopping/OnlineShopping/Program.cs
@@ -0,0 +1,250 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace Shopping_System
+{
+
+    abstract class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public abstract string DisplayDetails();
+
+        public abstract double CalculatePrice();
+
+
+    }
+
+    interface IPurchasable
+    {
+        int Quantity { get; set; }
+        double CalculateTotalPrice();
+        string DisplayProductDetails();
+    }
+
+
+    class Electronic : Product
+    {
+        public double ShippingCost { get; set; }
+        public int Quantity { get; set; }
+
+        public static double GST = 1.2;
+
+        public Electronic(int id, string name, double price, double shippingCost, int quantity)
+            : base(id, name, price)
+        {
+            ShippingCost = shippingCost;
+            Quantity = quantity;
+        }
+
+        public override string DisplayDetails()
+        {
+            return $"Id : {Id}\t Name: {Name}\t Price: {Price}\t Quantity: {Quantity}\t ShippingCost: {ShippingCost}";
+        }
+
+        public override double CalculatePrice()
+        {
+            return (Price * Quantity * GST) + ShippingCost;
+        }
+    }
+
+    class Apparel : Product
+    {
+        public int Quantity { get; set; }
+
+        public static double GST = 1.8;
+
+        public Apparel(int id, string name, double price, int quantity)
+              : base(id, name, price)
+        {
+            Quantity = quantity;
+        }
+
+        public override string DisplayDetails()
+        {
+            return $"Id : {Id}\t Name: {Name}\t Price: {Price}\t Quantity: {Quantity}";
+        }
+
+        public override double CalculatePrice()
+        {
+            return Price * Quantity * GST;
+        }
+
+
+    }
+
+    class Laptop : IPurchasable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double ShippingCost { get; set; }
+        public int Quantity { get; set; }
+
+        public static double GST = 1.2;
+        public double CalculateTotalPrice()
+        {
+            return (Price * Quantity * GST) + ShippingCost;
+        }
+
+        public string DisplayProductDetails()
+        {
+            return $"Id : {Id}\t Name: {Name}\t Price: {Price}\t Quantity: {Quantity}\t ShippingCost: {ShippingCost}";
+        }
+    }
+
+    class Clothing : IPurchasable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Size { get; set; }
+
+        public static double GST = 1.8;
+        public double CalculateTotalPrice()
+        {
+            return Price * Quantity * GST;
+        }
+
+        public string DisplayProductDetails()
+        {
+            return $"Id : {Id}\t Name: {Name}\t Price: {Price}\t Quantity: {Quantity}";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IPurchasable> products = new List<IPurchasable>();
+
+
+            products.Add(new Laptop()
+            {
+                Id = 1,
+
+                Name = "Dell Vistro",
+
+                Price = 50000,
+                ShippingCost = 150,
+
+                Quantity = 6
+
+            });
+
+            products.Add(new Clothing()
+            {
+                Id = 2,
+                Name = "Jeans",
+                Price = 2000,
+                Size = 34,
+                Quantity = 4,
+            });
+            Console.WriteLine("Shopping Cart Contents:");
+            Console.WriteLine();
+
+            for (int i = 0; i < products.Count; i++)
+
+            {
+
+                Console.WriteLine($"[{i + 1}] {products[i].DisplayProductDetails()}");
+
+            }
+
+            Console.WriteLine();
+
+
+
+
+
+            Console.Write("Enter the product id to purchase the product: ");
+
+            int productNumber = int.Parse(Console.ReadLine());
+
+
+
+            if (productNumber < 1 || productNumber > products.Count)
+
+            {
+
+                Console.WriteLine("Product number is not correct. Please try again");
+
+                return;
+
+            }
+
+
+
+            IPurchasable selectedProduct = products[productNumber - 1];
+
+
+
+            Console.WriteLine();
+
+            Console.WriteLine("Details of selected product:");
+
+            Console.WriteLine(selectedProduct.DisplayProductDetails());
+
+            while (true)
+            {
+
+
+
+                Console.Write("Quantity of product to purchase: ");
+
+                int quantity = int.Parse(Console.ReadLine());
+
+
+
+
+
+                if (quantity > selectedProduct.Quantity)
+                {
+                    Console.WriteLine("No enough quantity available. Please enter quantity again");
+                    continue;
+                }
+                else
+                {
+                    selectedProduct.Quantity = quantity;
+                }
+
+
+
+                Console.WriteLine();
+
+                Console.WriteLine("Product Details With Price:");
+
+                Console.WriteLine(selectedProduct.DisplayProductDetails());
+
+                Console.WriteLine($"Total amount to pay for {quantity} item : {selectedProduct.CalculateTotalPrice()}");
+
+
+                Console.WriteLine("Thank you for shopping with us!");
+
+                Console.WriteLine("Press any key to exit...");
+
+                Console.ReadKey();
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
